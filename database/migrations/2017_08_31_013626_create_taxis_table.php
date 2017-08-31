8a0f894802d4eb9af1084682572499eded0ea720
YiTingLee <ranye0719@gmail.com>
2017-08-31 10:42:22 +0800

    my first commit

---
+++ database/migrations/2017_08_31_013626_create_taxis_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaxisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('car_id');
+            $table->string('lat');
+            $table->string('lng');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('taxis');
+    }
+}
